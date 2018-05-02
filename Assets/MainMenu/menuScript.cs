0e8361affadb6ff4c6780abd9167f14ab10ac68c
PartyTaylor <33302487+PartyTaylor@users.noreply.github.com>
2018-05-02 18:08:11 -0500

    Small update

--- Assets/MainMenu/menuScript.cs
+++ Assets/MainMenu/menuScript.cs
@@ -8,12 +8,14 @@ public class menuScript : MonoBehaviour
 {
     public Canvas quitMenu;
     public Button startText;
+    public Button startTextTeacher;
     public Button exitText;
 	
 	void Start ()
     {
         quitMenu = quitMenu.GetComponent<Canvas>();
         startText = startText.GetComponent<Button>();
+        startTextTeacher = startTextTeacher.GetComponent<Button>();
         exitText = exitText.GetComponent<Button>();
         quitMenu.enabled = false;
 	}
@@ -23,12 +25,14 @@ public class menuScript : MonoBehaviour
         quitMenu.enabled = true;
         startText.enabled = false;
         exitText.enabled = false;
+        startTextTeacher.enabled = false;
     }
 	
     public void NoPress()
     {
         quitMenu.enabled = false;
         startText.enabled = true;
+        startTextTeacher.enabled = true;
         exitText.enabled = true;
     }
 
@@ -36,6 +40,12 @@ public class menuScript : MonoBehaviour
     {
         SceneManager.LoadScene("loading1");
     }
+
+    public void StartLevelTeacher()
+    {
+        SceneManager.LoadScene("loading2");
+    }
+
     public void ExitGame()
     {
         Application.Quit();
