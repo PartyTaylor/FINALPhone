b0c6e3210c25e4f0d4b6f5c0bebee8786e6b85cf
Tobirenn <37786386+Tobirenn@users.noreply.github.com>
2018-05-01 21:40:00 -0500

    stamina

--- Assets/Scripts/Staminabar.cs
+++ Assets/Scripts/Staminabar.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class Staminabar : MonoBehaviour {
 
-    public float staminaDrain = 1F;
+    public float staminaDrain = 5F;
     public float stamina = 100;
     public float maxstamina = 100;
     public Image currentstamina;
+
+    protected CharacterController cm;
+    protected Vector3 move = Vector3.zero;
+
+    public float walkspeed = 1f;
 
     public bool run;
 
     // Use this for initialization
     void Start ()
     {
-		
+        cm = gameObject.GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
@@ -24,6 +31,12 @@ public class Staminabar : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             run = true; 
+            if (stamina == 0)
+            {
+                run = false;
+                cm.Move(move * walkspeed * Time.deltaTime);
+                Update();
+            }
         }
         if(Input.GetKeyUp(KeyCode.LeftShift))
         {
@@ -38,14 +51,14 @@ public class Staminabar : MonoBehaviour {
         }
         if (run == false && stamina >= 0)
         {
-            stamina += Time.deltaTime * staminaDrain;
+            stamina += Time.deltaTime * 2;
             float ratio = stamina / maxstamina;
             currentstamina.rectTransform.localScale = new Vector3(ratio, 1, 1);
         }
-        if (stamina == 0)
+        if (stamina <= 0 )
         {
             run = false;
-            stamina -= Time.deltaTime * staminaDrain;
+            stamina += Time.deltaTime * 2;
             float ratio = stamina / maxstamina;
             currentstamina.rectTransform.localScale = new Vector3(ratio, 1, 1);
         }
